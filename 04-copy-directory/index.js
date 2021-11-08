0a3a0d25775a50326265fc260dd358515508e406
KseniyaSokolovskaya <deff.1990@gmail.com>
2021-11-08 18:58:17 +0300

    refactor: refactor 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,30 +1,27 @@
 const path = require('path');
 const { readdir, mkdir, rmdir, copyFile } = require('fs').promises;
+exports = module.exports = {};
 
 const initialDirPath = path.resolve(__dirname, 'files');
 const copyDirPath = path.resolve(__dirname, 'files-copy');
 
-const reWriteDirectory = async () => {
-  await rmdir(copyDirPath, { recursive: true });
-  await mkdir(copyDirPath, { recursive: true });
-};
-
-async function copyFiles(name) {
-  const initialFilePath = path.resolve(initialDirPath, name);
-  const copyFilePath = path.resolve(copyDirPath, name);
-  await copyFile(initialFilePath, copyFilePath, null, (err) => {
-    if (err) throw err;
-  });
-}
-
-async function getDirectoryFiles() {
-  const files = await readdir(initialDirPath, { withFileTypes: true, });
-  files.forEach(file => {
-    file.isFile() && copyFiles(file.name);
-  });
+async function copyDir(fromPath, toPath) {
+  const entries = await readdir(fromPath, { withFileTypes: true });
+  await mkdir(toPath, { recursive: true });
+  for (let entry of entries) {
+    const srcPath = path.join(fromPath, entry.name);
+    const destPath = path.join(toPath, entry.name);
+    if (entry.isDirectory()) {
+      await copyDir(srcPath, destPath);
+    } else {
+      await copyFile(srcPath, destPath);
+    }
+  }
 }
 
 (async () => {
-  await reWriteDirectory();
-  await getDirectoryFiles();
+  await rmdir(copyDirPath, { recursive: true })
+  await copyDir(initialDirPath, copyDirPath);
 })();
+
+exports.copyDir = copyDir;
