2460d0e183db58b6caa0dfcba84cf59b361ac17e
KseniyaSokolovskaya <deff.1990@gmail.com>
2021-11-08 19:15:32 +0300

    refactor: refactor 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,18 +4,19 @@ const { readdir, readFile, writeFile } = require('fs').promises;
 const initialDirPath = path.resolve(__dirname, 'styles');
 const EXTENSION = '.css';
 const destFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
+exports = module.exports = {};
 
-
-async function getDirectoryFilesPath() {
-  const files = await readdir(initialDirPath, { withFileTypes: true, }, (err, files) => {
+async function getDirectoryFilesPath(dirPath, ext) {
+  const files = await readdir(dirPath, { withFileTypes: true, }, (err, files) => {
     if (err) {
       console.log('err', err);
     }
-    return files.filter(file => path.extname(file.name) === EXTENSION)
-      .map(file => path.join(initialDirPath, file.name));
+    return files.filter(file => path.extname(file.name) === ext)
+      .map(file => path.join(dirPath, file.name));
   });
-  return files.filter(file => path.extname(file.name) === EXTENSION)
-    .map(file => path.join(initialDirPath, file.name));
+
+  return files.filter(file => path.extname(file.name) === ext)
+    .map(file => path.join(dirPath, file.name));
 }
 
 async function concatFiles(array) {
@@ -25,8 +26,13 @@ async function concatFiles(array) {
   return result;
 }
 
-(async () => {
-  let cssFilesPath = await getDirectoryFilesPath();
+async function buildFiles(dirPath, destFilePath, ext) {
+  let cssFilesPath = await getDirectoryFilesPath(dirPath, ext);
+
   let buildFile = await concatFiles(cssFilesPath);
   writeFile(destFilePath, buildFile, 'utf-8');
-})();
+}
+
+buildFiles(initialDirPath, destFilePath, EXTENSION);
+
+exports.buildFiles = buildFiles;
